56d0b4d66146886d28ef0c3be8d16ae7a0a4495a
wjdtj9656 <wjdtj9656@naver.com>
2022-12-10 15:53:57 +0900

    Time: 206 ms (70.06%), Space: 47.9 MB (42.26%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function(n) {
+    const dp = [];
+    dp[0] = 0;
+    for(let i=1; i<=n; i++){
+        dp[i] = 10e9;
+        for(let j=1; j*j<=i; j++){
+            dp[i] = Math.min(dp[i],dp[i-j*j]+1);
+        }
+    }
+    return dp[n];
+};
