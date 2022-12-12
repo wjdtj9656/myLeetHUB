8961c2cb052c2a3072121bb7879a4376773b1e86
wjdtj9656 <wjdtj9656@naver.com>
2022-12-12 18:41:20 +0900

    Time: 425 ms (30.56%), Space: 47.4 MB (65.03%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    const merge = (list1, list2) =>{
+        if(!list1 || !list2) return list1 || list2;
+        let node = {};
+        const root = node;
+        while(list1 && list2){
+            if(list1.val <= list2.val){
+                node.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                node.next = list2;
+                list2 = list2.next;
+            }
+            node = node.next;
+        }
+        if(list1) node.next = list1;
+        if(list2) node.next = list2;
+        return root.next;
+    }
+    let root = lists[0];
+    for(let i=1; i<lists.length; i++){
+        root = merge(root,lists[i]);
+    }
+    return root || null;
+};
