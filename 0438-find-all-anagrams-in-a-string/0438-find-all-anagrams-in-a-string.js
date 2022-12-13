1016ff2d66cbf99b4c6d396c41b480c801071411
wjdtj9656 <wjdtj9656@naver.com>
2022-12-14 02:02:35 +0900

    Time: 392 ms (25.17%), Space: 51.1 MB (13.73%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function(s, p) {
+    p = p.split('').sort().join('');
+    let [left,right] = [0,0];
+    const len = s.length;
+    const sMap = new Map();
+    const window = new Map();
+    const result = [];
+    for(let str of p){
+        if(sMap.has(str)) sMap.set(str,sMap.get(str)+1);
+        else sMap.set(str,1);
+    }
+    let valid = 0;
+    while(right < len){
+        let nr = s[right];
+        right++;
+        if(sMap.has(nr)){
+            if(window.has(nr)) window.set(nr,window.get(nr)+1);
+            else window.set(nr,1);
+            if(sMap.get(nr) === window.get(nr)) valid++;
+        }
+        while(right - left >= p.length){
+            if(valid === [...sMap].length) result.push(left);
+            let nl = s[left];
+            left++;
+            if(sMap.has(nl)){
+                if(sMap.get(nl) === window.get(nl)) valid--;
+                window.set(nl,window.get(nl)-1);
+            }
+        }
+    }
+    return result;
+};
