73aa35ca8cd7b71b201df2e5115c4cc6a3cd60ee
wjdtj9656 <wjdtj9656@naver.com>
2022-12-14 20:12:15 +0900

    Time: 66 ms (88.51%), Space: 42.3 MB (14.47%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    const dp = new Array(nums.length);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0],nums[1]);
+    for(let i=2; i<dp.length; i++){
+        dp[i] = Math.max(dp[i-1],dp[i-2] + nums[i]);
+    }
+    return dp[nums.length-1];
+};
