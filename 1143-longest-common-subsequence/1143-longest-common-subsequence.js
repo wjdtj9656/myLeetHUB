8d9cfe8fb45052e81238480d6abcb7d46843204d
wjdtj9656 <wjdtj9656@naver.com>
2022-12-15 21:35:10 +0900

    Time: 95 ms (94.76%), Space: 56.2 MB (41.84%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+    const dp = Array.from(Array(text1.length+1),()=>new Array(text2.length+1).fill(0));
+    for(let i=1; i<=text1.length; i++){
+        for(let j=1; j<=text2.length; j++){
+            if(text1.charAt(i-1) !== text2.charAt(j-1)){
+                dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+            else{
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+        }
+    }
+    return dp[text1.length][text2.length]
+};
