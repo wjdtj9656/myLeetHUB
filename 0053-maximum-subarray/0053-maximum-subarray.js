e66e5dabf4143a9e43d520e1a06f4189360bfa90
wjdtj9656 <wjdtj9656@naver.com>
2022-12-17 01:19:02 +0900

    Time: 85 ms (94.19%), Space: 49.8 MB (97.17%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let max = nums[0];
+    for(let i=1; i<nums.length; i++){
+        nums[i] = Math.max(0,nums[i-1]) + nums[i];
+        max = Math.max(max, nums[i]);
+    }
+    return max;
+};
