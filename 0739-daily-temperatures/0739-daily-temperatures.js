531a332c9468e430c36ec7df7d75f4832776824b
wjdtj9656 <wjdtj9656@naver.com>
2022-12-18 17:48:33 +0900

    Time: 260 ms (98.18%), Space: 64.6 MB (84.73%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -5,15 +5,13 @@
 var dailyTemperatures = function(temperatures) {
     const len = temperatures.length;
     const result = new Array(len).fill(0);
+    const stack = [];
     for(let i=0; i<len; i++){
-        let cnt = 0;
-        for(let j=i+1; j<len; j++){
-            cnt++;
-            if(temperatures[i] < temperatures[j]){
-                result[i] = cnt;
-                break;
-            }
+        while(stack.length && temperatures[i] > temperatures[stack[stack.length-1]]){
+            let temp = stack.pop();
+            result[temp] = i-temp;
         }
+        stack.push(i);
     }
     return result;
 };
