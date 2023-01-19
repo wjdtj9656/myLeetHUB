da6c3b9351b7d5566fb408eb27fe6c175df42c89
wjdtj9656 <wjdtj9656@naver.com>
2023-01-19 09:37:36 +0900

    Time: 192 ms (68.42%), Space: 47.4 MB (52.43%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.js
+++ 0279-perfect-squares/0279-perfect-squares.js
@@ -1,15 +1,25 @@
-/**
+/** 
  * @param {number} n
  * @return {number}
  */
+//1 4 9 16 25 36 49 64 81
 var numSquares = function(n) {
-    const dp = [];
+    const q = [];
+    const dp = new Array(n).fill(Infinity);
     dp[0] = 0;
     for(let i=1; i<=n; i++){
-        dp[i] = 10e9;
-        for(let j=1; j*j<=i; j++){
-            dp[i] = Math.min(dp[i],dp[i-j*j]+1);
+        let num = Math.sqrt(i);
+        if(num%1 === 0){
+            q.push(i);
+            dp[i] = 1;
+            continue;
         }
+        dp[i] = dp[i-1] + 1;
+        for(let j=0; j<q.length; j++){
+            let index = q[j];
+            dp[i] = Math.min(dp[i], dp[i-index] + 1);
+        }
+    
     }
     return dp[n];
 };
