7ce176a755339af98cea8716217ee0f40cd93504
wjdtj9656 <wjdtj9656@naver.com>
2023-03-01 17:02:45 +0900

    Time: 226 ms (78.14%), Space: 71 MB (81.97%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindromeSubseq = function(s) {
+    let n = s.length;
+    const dp = Array.from(Array(n),()=>new Array(n).fill(0));
+    for(let i=0; i<n; i++) dp[i][i] = 1;
+    
+    for(let i=n-2; i>=0; i--){
+        for(let j=i+1; j<n; j++){
+            if(s[i] === s[j]) dp[i][j] = dp[i+1][j-1] + 2;
+            else{
+                dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[0][n-1];
+};
