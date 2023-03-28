e005ea5a6d0a72085c35d91fdaaf59c24cda2b26
wjdtj9656 <wjdtj9656@naver.com>
2023-03-28 22:57:56 +0900

    Time: 114 ms (32.57%), Space: 49.2 MB (45.31%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -1,32 +1,15 @@
-/**
- * @param {number[][]} intervals
- * @return {number[][]}
- */
-var merge = function(intervals) {
-    let left = 0;
-    let right = 1;
-    intervals.sort((a,b)=>{
-        if(a[0] === b[0]){
-            return a[1] - b[1];
-        }
-        return a[0] - b[0];
-    })
-    let now = [intervals[0]];
-    const result = [];
-    while(left !== intervals.length){
-        if(now.length === 0){
-            now.push(intervals[left]);
-        }
-        if(right < intervals.length && intervals[left][1] >= intervals[right][0] ){
-            now[0][0] = intervals[left][0];
-            now[0][1] = Math.max(intervals[right][1],intervals[left][1]);
-            right++;
-        }
-        else{
-            result.push(now.shift());
-            left = right;
-            right++;
-        }
+function merge(intervals) {
+  if (!intervals.length) return intervals
+  intervals.sort((a, b) => a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1])
+  var prev = intervals[0]
+  var res = [prev]
+  for (var curr of intervals) {
+    if (curr[0] <= prev[1]) {
+      prev[1] = Math.max(prev[1], curr[1])
+    } else {
+      res.push(curr)
+      prev = curr
     }
-    return result;
-};
+  }
+  return res
+}
