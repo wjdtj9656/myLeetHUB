0489c814d8e2929c5a9804b1e01013dd164295d9
wjdtj9656 <wjdtj9656@naver.com>
2023-05-27 17:28:54 +0900

    Time: 143 ms (45.61%), Space: 64 MB (70.18%) - LeetHub

---
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} arrays
+ * @return {number}
+ */
+var maxDistance = function(arrays) {
+    let result = -Infinity;
+    let min = arrays[0][0];
+    let max = arrays[0].pop();
+    
+    for(let i=1; i<arrays.length; i++){
+        let curMin = arrays[i][0];
+        let curMax = arrays[i].pop();
+        
+        result = Math.max(max - curMin, result);
+        result = Math.max(curMax - min, result);
+        
+        min = Math.min(min, curMin);
+        max = Math.max(max, curMax);
+    }
+    return result;
+};
