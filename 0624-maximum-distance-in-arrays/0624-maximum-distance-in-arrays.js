6a6aa8486b9073b49c2095ef3db8545458ce5067
wjdtj9656 <wjdtj9656@naver.com>
2023-06-25 02:47:35 +0900

    Time: 151 ms (28.26%), Space: 63.2 MB (98.91%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.js
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.js
@@ -6,13 +6,12 @@ var maxDistance = function(arrays) {
     let result = -Infinity;
     let min = arrays[0][0];
     let max = arrays[0].pop();
-    
     for(let i=1; i<arrays.length; i++){
         let curMin = arrays[i][0];
         let curMax = arrays[i].pop();
         
-        result = Math.max(max - curMin, result);
-        result = Math.max(curMax - min, result);
+        result = Math.max(result, curMax - min);
+        result = Math.max(result, max - curMin);
         
         min = Math.min(min, curMin);
         max = Math.max(max, curMax);
