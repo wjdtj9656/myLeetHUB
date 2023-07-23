6d998a94118bdbae0732046d5192b4baaeb975e0
wjdtj9656 <wjdtj9656@naver.com>
2023-07-24 07:08:09 +0900

    Time: 1269 ms (31.77%), Space: 0B (100.00%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,6 @@
+select employee_id, department_id
+from employee where primary_flag ='Y'
+union 
+select employee_id, department_id
+from employee 
+where employee_id in (select employee_id from employee group by employee_id having count(1) = 1)
