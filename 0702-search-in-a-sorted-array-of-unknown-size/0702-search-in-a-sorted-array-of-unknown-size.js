8c2d82659255d5a79aa6d09085e025e8d0fedca1
wjdtj9656 <wjdtj9656@naver.com>
2023-07-28 06:52:10 +0900

    Time: 58 ms (86.00%), Space: 45.2 MB (62.00%) - LeetHub

---
+++ 0702-search-in-a-sorted-array-of-unknown-size/0702-search-in-a-sorted-array-of-unknown-size.js
@@ -0,0 +1,39 @@
+/**
+ * // This is the ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function ArrayReader() {
+ *
+ *     @param {number} index
+ *     @return {number}
+ *     this.get = function(index) {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {ArrayReader} reader
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (reader, target) {
+    let left = 0;
+    let right = 10**4;
+    while(left <= right){
+        if(!reader.get(right)){
+            right--;
+            continue;
+        }
+        let mid = (left + right) >> 1;
+        if(reader.get(mid) == target){
+            return mid;
+        }
+        else if(reader.get(mid) < target){
+            left = mid + 1;
+        }
+        else if(reader.get(mid) > target){
+            right = mid - 1;
+        }
+    }
+    return -1;
+};
