ec8db5e82ec64b6840e311d68d1ff564b208e604
wjdtj9656 <wjdtj9656@naver.com>
2023-07-29 06:13:00 +0900

    Update 0033-search-in-rotated-sorted-array.js

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target);
+    let arr = [...nums];
+    arr.sort((a,b)=>a-b);
+    let left = 0;
+    let right = arr.length-1;
+    while(left <= right){
+        let mid = (left + right) >> 1;
+        if(arr[mid] == target){
+            return nums.indexOf(target);
+        }
+        else if(arr[mid] < target){
+            left = mid + 1;
+        }
+        else if(arr[mid] > target){
+            right = mid - 1;
+        }
+    }
+    return -1;
 };
