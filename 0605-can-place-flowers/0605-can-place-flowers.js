56f310481ef9199170dcad84945a0afaba97edc1
wjdtj9656 <wjdtj9656@naver.com>
2023-08-19 18:14:08 +0900

    Time: 64 ms (52.64%), Space: 44 MB (79.36%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -4,40 +4,15 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
-  let cnt = 0;
-    for(let i=0; i<flowerbed.length; i++){
-      if(flowerbed[i] == 1){
-        if(i-1 >= 0){
-          flowerbed[i-1] = 2;
-        }
-        else if(i+1 < flowerbed.length){
-          flowerbed[i+1] = 2; 
-        }
-      }
-      if(flowerbed[i] == 0){
-        if(i==0){
-          if(flowerbed.length == 1) cnt++;
-          if(flowerbed[i+1] != 1){
-            flowerbed[i] = 1;
-            flowerbed[i+1] == 2;
-            cnt++;
-          }
-        }
-        else if(i== flowerbed.length-1){
-          if(flowerbed[i-1] != 1) {
-            flowerbed[i] = 1;
-            cnt++;
-          }
-        }
-        else{
-          if(flowerbed[i-1] != 1 && flowerbed[i+1] != 1){
-            flowerbed[i] = 1;
-            flowerbed[i+1] = 2;
-            cnt++;
-          }
-        }
-      }
-      if(cnt >= n) return true;
+  for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+    if (
+      flowerbed[i] === 0 &&
+      flowerbed[i - 1] !== 1 &&
+      flowerbed[i + 1] !== 1
+    ) {
+      n--;
+      i++;
     }
-    return false;
+  }
+  return n === 0;
 };
