296c058c67544e612eaa912276738edc82d2f5f3
wjdtj9656 <wjdtj9656@naver.com>
2023-08-28 06:47:38 +0900

    Time: 227 ms (43.75%), Space: 156.7 MB (5%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.js
+++ 0403-frog-jump/0403-frog-jump.js
@@ -3,27 +3,34 @@
  * @return {boolean}
  */
 var canCross = function(stones) {
-  if(stones[1] !== 1) return false;
-  const m = stones.length;
-  const dp = new Array(m).fill(-1).map(v=>new Array(m).fill(-1));
+  const len = stones.length;
+  const dp = new Array(len).fill(-1).map(v=>new Array(len).fill(-1));
   const map = new Map();
-  for(let i=0; i<stones.length; i++){
-    map.set(stones[i], i);
+  for(let i=0; i<len; i++){
+    map.set(stones[i],i);
   }
-  return search(stones, m, 0, 0, dp, map);
+  return search(stones, len, 0, 0, map, dp);
 };
 
-const search = (stones, n, index, prevJump, dp, map) =>{
-  if(index == n-1) return true;
+const search = (stones, len, index, prevJump, map, dp) =>{
+  if(index == len-1) return true;
   if(dp[index][prevJump] !== -1){
     return dp[index][prevJump] == 1;
   }
   let ans = false;
   for(let i=prevJump-1; i<=prevJump+1; i++){
     if(i > 0 && map.has(stones[index] + i)){
-      ans = ans || search(stones, n, map.get(stones[index] + i), i,dp,map);
+      ans = ans || search(stones, len, map.get(stones[index] + i), i, map, dp);
     }
   }
-  dp[index][prevJump] = (ans ? 1 : 0);
+  dp[index][prevJump] = ans;
   return ans;
 }
+
+
+
+
+
+
+
+
