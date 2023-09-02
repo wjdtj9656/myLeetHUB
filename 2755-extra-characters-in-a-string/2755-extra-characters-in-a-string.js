5e3397d3f1507c7649e8e7a677624d41e93c742b
wjdtj9656 <wjdtj9656@naver.com>
2023-09-02 20:20:35 +0900

    Time: 241 ms (24.49%), Space: 49.7 MB (100%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.js
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.js
@@ -3,22 +3,22 @@
  * @param {string[]} dictionary
  * @return {number}
  */
-var minExtraChar = function(s, dictionary) {
+var minExtraChar = function(s, dict) {
     const memo = new Map();
-    const dfs = (rest) => {
+    const search = (rest) =>{
         if(memo.has(rest)) return memo.get(rest);
-        let maxi = 0;
-        for(let i=0; i<dictionary.length; i++){
-            let index = rest.indexOf(dictionary[i]);
+        let max = 0;
+        for(let i=0; i<dict.length; i++){
+            const index = rest.indexOf(dict[i]);
             if(index >= 0){
-                let left = rest.slice(0,index) 
-                let right = rest.slice(index+dictionary[i].length,rest.length);
-                const res = dfs(left) + dfs(right) + dictionary[i].length;
-                maxi = Math.max(res,maxi);
+                let left = rest.slice(0, index);
+                let right = rest.slice(index+dict[i].length, rest.length);
+                const res = search(left) + search(right) + dict[i].length;
+                max = Math.max(max, res);
             }
         }
-        memo.set(rest, maxi);
-        return maxi;
+        memo.set(rest,max);
+        return max;
     }
-    return s.length - dfs(s);
+    return s.length - search(s);
 };
