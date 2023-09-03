78ab2bd43235da7a3be2c3455beb8815588fb05f
wjdtj9656 <wjdtj9656@naver.com>
2023-09-03 13:43:13 +0900

    Time: 54 ms (60.69%), Space: 42.3 MB (41.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -4,24 +4,8 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    let answer = 0;
-    // const dfs = (x,y) =>{
-    //     if(x<0 || y<0 || x>=m || y>=n) return;
-    //     if(x == m-1 && y == n-1){
-    //         answer++;
-    //         return;
-    //     }
-    //     dfs(x+1,y);
-    //     dfs(x,y+1);
-    // }
-    // dfs(0,0);
-    const map = Array.from(Array(m),()=>new Array(n));
-    for(let i=0; i<n; i++){
-        map[0][i] = 1;
-    }
-    for(let i=0; i<m; i++){
-        map[i][0] = 1;
-    }
+    const map = new Array(m).fill(1).map(v=>new Array(n).fill(1));
+
     for(let i=1; i<m; i++){
         for(let j=1; j<n; j++){
             map[i][j] = map[i-1][j] + map[i][j-1];
