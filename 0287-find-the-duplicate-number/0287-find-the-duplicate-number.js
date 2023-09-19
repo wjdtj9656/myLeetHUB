37658e7070910d931ec3442b247191874907b387
wjdtj9656 <wjdtj9656@naver.com>
2023-09-19 21:43:13 +0900

    Time: 85 ms (49.26%), Space: 61.3 MB (23.4%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    const set = new Set();
+    for(let num of nums){
+        if(set.has(num)){
+            return num
+        }
+        else set.add(num);
+    }
+};
