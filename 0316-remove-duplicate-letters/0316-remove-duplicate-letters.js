8683834b34aa64f8d1fa6cbb4297e1924bf0e0c1
wjdtj9656 <wjdtj9656@naver.com>
2023-09-26 20:47:08 +0900

    Time: 119 ms (5.4%), Space: 50.1 MB (5.4%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicateLetters = function(s) {
+    const map = new Map();
+    for(let i=0; i<s.length; i++){
+        map.set(s[i],(map.get(s[i]) || 0)+1);
+    }
+    console.log(map)
+    let pos = 0;
+    for(let i=0; i<s.length; i++){
+        map.set(s[i],(map.get(s[i])-1));
+        if(s[i] < s[pos]) pos = i;
+        if(map.get(s[i]) == 0) {
+            break;
+        }
+    }
+    return s.length === 0? "" : s[pos] + removeDuplicateLetters(s.slice(pos+1).replaceAll(s[pos],""));
+};
