c1d5bfc98552890f28c9e45d6c510aab0cf2e6cc
wjdtj9656 <wjdtj9656@naver.com>
2023-10-05 06:49:55 +0900

    Time: 59 ms (55.26%), Space: 43.3 MB (52.63%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.js
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.js
@@ -3,11 +3,14 @@
  * @return {number}
  */
 var numSub = function(s) {
-    let res = 0;
-    let one = 0;
-    for(let num of s){
-        if(num === "1") one++, res+=one;
-        else one = 0;
+    let cnt = 0;
+    let result = 0;
+    for(let i=0; i<=s.length; i++){
+        if(s[i] == '1') cnt++;
+        else{
+            result += ((cnt + 1) * cnt / 2) % (10**9 + 7);
+            cnt = 0;
+        }
     }
-    return res %(10**9+7);
+    return result
 };
