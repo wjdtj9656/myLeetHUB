5fad4173139e3bed9e7a2efc89516803d141a24f
wjdtj9656 <wjdtj9656@naver.com>
2023-10-17 06:51:43 +0900

    Time: 76 ms (5.31%), Space: 46.1 MB (5.07%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    const arr = new Array(rowIndex+1).fill(1).map(v=>new Array(rowIndex+1).fill(1));
+    console.log(arr)
+    for(let i=0; i<=rowIndex; i++){
+        for(let j=1; j<i; j++){
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+        }
+    }
+    return arr[rowIndex]
+};
