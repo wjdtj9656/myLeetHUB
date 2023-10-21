ca8781402643f084de68559a835ae6cacc8b18d3
wjdtj9656 <wjdtj9656@naver.com>
2023-10-21 23:37:44 +0900

    Time: 282 ms (73.33%), Space: 51.1 MB (82.22%) - LeetHub

--- 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.js
+++ 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.js
@@ -5,46 +5,34 @@
 var shortestDistance = function(grid) {
     const m = grid.length;
     const n = grid[0].length;
-    let dis = new Array(m).fill(Infinity).map(v=>new Array(n).fill(Infinity));
-    const result = new Array(m).fill(-1).map(v=>new Array(n).fill(-1));
+    let dis = new Array(m).fill(0).map(v=>new Array(n).fill(0));
     const dx = [-1,0,1,0];
     const dy = [0,-1,0,1];
+    let cur = 0;
+    let result = Infinity;
     const calDis = (x,y) =>{
         const q = [[x,y,0]];
-        const visit = new Set();
-        visit.add(`${x}-${y}`);
+        result = Infinity;
         while(q.length){
             let [nowX,nowY,cnt] = q.shift();
-            dis[nowX][nowY] = cnt;
             for(let i=0; i<4; i++){
                 let nx = nowX + dx[i];
                 let ny = nowY + dy[i];
-                if(nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] == 2 || visit.has(`${nx}-${ny}`) || grid[nx][ny] == 1) continue;
-                visit.add(`${nx}-${ny}`);
+                if(nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != cur) continue;
                 q.push([nx,ny,cnt+1]);
+                dis[nx][ny] += cnt+1; 
+                result = Math.min(result, dis[nx][ny]);
+                grid[nx][ny]--;
             }
         }
-        for(let i=0; i<m; i++){
-            for(let j=0; j<n; j++){
-                result[i][j] += dis[i][j];
-            }
-        }
+        cur--;
     }
     for(let i=0; i<m; i++){
         for(let j=0; j<n; j++){
             if(grid[i][j] == 1){
                 calDis(i,j);
-                dis = new Array(m).fill(Infinity).map(v=>new Array(n).fill(Infinity));
             }
         }
     }
-    let res = Infinity;
-    for(let i=0; i<m; i++){
-        for(let j=0; j<n; j++){
-            if(grid[i][j] == 0 && result[i][j] != -1){
-                res = Math.min(res, result[i][j])
-            }
-        }
-    }
-    return res == Infinity? -1:res+1;
+    return result == Infinity ? -1:result;
 };
