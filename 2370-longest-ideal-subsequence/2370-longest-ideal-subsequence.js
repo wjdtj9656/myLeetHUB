63d254b5e84cff9408b64a610fe230f4c28883c3
wjdtj9656 <wjdtj9656@naver.com>
2024-04-26 07:06:08 +0900

    Time: 149 ms (100%), Space: 52 MB (100%) - LeetHub

---
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var longestIdealString = function(s, k) {
+    let dp = new Array(128).fill(0);
+    let maxLen = 0;
+
+    for (let char of s) {
+        let ascii = char.charCodeAt(0);
+        let maxForChar = 0;
+
+        for (let i = Math.max(0, ascii - k); i <= Math.min(127, ascii + k); i++) {
+        maxForChar = Math.max(maxForChar, dp[i]);
+        }
+
+        dp[ascii] = maxForChar + 1; 
+        maxLen = Math.max(maxLen, dp[ascii]); 
+    }
+
+    return maxLen;
+};
