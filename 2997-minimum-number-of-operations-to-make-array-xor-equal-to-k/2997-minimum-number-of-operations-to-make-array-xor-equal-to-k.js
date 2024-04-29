5c0c1e74aa498f48ff3bca0a33df30b574e9f1df
wjdtj9656 <wjdtj9656@naver.com>
2024-04-30 07:13:28 +0900

    Time: 81 ms (45.45%), Space: 61.6 MB (9.09%) - LeetHub

---
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    let value = k
+    for (let item of nums){
+        value ^= item
+    }
+
+    let answer = 0
+    while (value > 0){
+        value&=(value-1)
+        answer++
+    }
+        
+    return answer
+};
