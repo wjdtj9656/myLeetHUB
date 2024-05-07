afbd87dac7b18524e080943981b4661ca902b8d3
wjdtj9656 <wjdtj9656@naver.com>
2024-05-07 21:18:25 +0900

    Time: 122 ms (12.3%), Space: 64.5 MB (31.61%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,16 +3,34 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let result = []
-    let left = 1;
-    let right = 1;
-    for(let i=nums.length-1; i>=0; i--){
-       result[i] = right;
-        right *= nums[i];
-    }
+    let zeroCnt = 0;
+    let zeroIndex =[];
     for(let i=0; i<nums.length; i++){
-        result[i] *= left;
-        left *= nums[i];
+        if(nums[i] == 0){
+            zeroCnt++;
+            zeroIndex.push(i);
+        }
     }
-    return result;
+    if(zeroCnt == 0){
+        let sum = nums.reduce((a,b)=>a*b,1);
+        const arr = new Array(nums.length).fill(sum);
+        for(let i=0; i<arr.length; i++){
+            arr[i] /= nums[i];
+        }
+        return arr;
+    }
+    else if(zeroCnt == 1){
+        let sum = nums.reduce((a,b)=>{
+            if(b != 0){
+                return a*b;
+            }
+            return a;
+        },1);
+        const result = new Array(nums.length).fill(0);
+        result[zeroIndex] = sum;
+        return result;
+    }
+    else{
+        return new Array(nums.length).fill(0);
+    }
 };
