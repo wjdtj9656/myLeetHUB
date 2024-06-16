10e45748497a26ce7fc7b456a452403e760605ed
wjdtj9656 <wjdtj9656@naver.com>
2024-06-16 22:42:53 +0900

    Time: 46 ms (90.9%), Space: 48.7 MB (87.17%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    let ans = 0;
+    const sorted = heights.slice().sort((a, b) => a - b);
+    for (let i = 0; i < heights.length; i++) {
+        if (sorted[i] !== heights[i]) {
+            ans++;
+        }
+    }
+    return ans;    
+};
