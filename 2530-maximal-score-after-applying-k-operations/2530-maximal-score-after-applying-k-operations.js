9572f4ef8463539557530fd2f140969b2ac0cfec
wjdtj9656 <wjdtj9656@naver.com>
2024-10-14 22:13:19 +0900

    Time: 152 ms (100%), Space: 72.4 MB (88%) - LeetHub

---
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.js
@@ -0,0 +1,72 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxKelements = function(nums, k) {
+    const heap = new MaxHeap();
+    let ans = 0;
+    for(let i=0; i<nums.length; i++){
+        heap.push(nums[i]);
+    }
+    for(let i=0; i<k; i++){
+        let num = heap.pop();
+        ans += num;
+        heap.push(Math.ceil(num/3));
+    }
+    return ans;
+};
+
+class MaxHeap{
+    constructor(){
+        this.heap=[null];
+    }
+    swap(a,b){
+        [this.heap[a],this.heap[b]] = [this.heap[b],this.heap[a]];
+    }
+    size(){
+        return this.heap.length;
+    }
+    pop(){
+        if(this.heap.length === 1) return;
+        const val = this.heap[1];
+        this.heap[1] = this.heap[this.size()-1];
+        this.heap.pop();
+        this.bubbledown();
+        return val;
+    }
+    push(val){
+        this.heap.push(val);
+        this.bubbleup();
+    }
+bubbledown(){
+    let index = 1;
+    while(index * 2 < this.size()){
+        let left = index * 2;
+        let right = index * 2 + 1;
+        let largest = left;
+
+        if(right < this.size() && this.heap[right] > this.heap[left]){
+            largest = right;
+        }
+        if(this.heap[index] < this.heap[largest]){
+            this.swap(index, largest);
+            index = largest;
+        } else {
+            break;
+        }
+    }
+}
+
+    bubbleup(){
+        let index = this.size()-1;
+        while(index > 1){
+            let p = index>>1;
+            if(this.heap[index] <= this.heap[p]){
+                break;
+            }
+            this.swap(index,p);
+            index = p;
+        }
+    }
+}
