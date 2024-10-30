5d316ac93aaabdf92e875fd380d17e2fba4916b2
wjdtj9656 <wjdtj9656@naver.com>
2024-10-30 21:22:21 +0900

    Time: 5 ms (47.47%), Space: 58.4 MB (46.91%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function(bills) {
+    let fives = 0, tens = 0;
+    for (let bill of bills) {
+        if (bill === 5) {
+            fives++;
+        } else if (bill === 10) {
+            if (fives === 0) return false;
+            fives--;
+            tens++;
+        } else { // bill === 20
+            if (tens > 0 && fives > 0) {
+                tens--;
+                fives--;
+            } else if (fives >= 3) {
+                fives -= 3;
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;    
+};
