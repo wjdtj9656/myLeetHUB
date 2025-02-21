48e0769b8d392caadfcbf1084bc87d3e023f816b
wjdtj9656 <wjdtj9656@naver.com>
2025-02-21 20:31:12 +0900

    Time: 170 ms (33.33%), Space: 71.2 MB (6.06%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.js
+++ 1416-restore-the-array/1416-restore-the-array.js
@@ -4,23 +4,20 @@
  * @return {number}
  */
 var numberOfArrays = function(s, k) {
-   let n = s.length;
+    const  n = s.length;
     const dp = new Array(n+1).fill(0);
-    let char = s.split("");
+    const char = s.split("");
+    const mod = 1000000007;
     dp[n] = 1;
-    let mod = 1000000007;
     for(let i=n-1; i>=0; i--){
         if(char[i] === "0") continue;
-        
-        let j = i+1;
+        let j= i+1;
         let str = "";
         str = str + char[i];
         while(j<=n && parseInt(str) <= k){
-            dp[i]+=dp[j];
-            dp[i]%=mod;
-            if(j<n){
-                str = str + char[j];
-            }
+            dp[i] += dp[j];
+            dp[i] %= mod;
+            if(j<n) str = str + char[j];
             j++;
         }
     }
