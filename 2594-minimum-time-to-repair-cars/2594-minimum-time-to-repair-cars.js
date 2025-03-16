50ad97de8012ee88ed364c21bd7b686e63b5d455
wjdtj9656 <wjdtj9656@naver.com>
2025-03-16 17:30:30 +0900

    Time: 50 ms (62.5%), Space: 61.7 MB (68.75%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.js
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.js
@@ -6,22 +6,22 @@
 var repairCars = function(ranks, cars) {
     let left = 1;
     let right = Math.max(...ranks) * cars * cars;
-    
     while(left < right){
-        let mid = Math.floor((left + right) / 2);
-        if(check(ranks, mid) < cars){
+        let mid = Math.floor((left + right)/2);
+        if(getSum(ranks,mid) < cars){
             left = mid + 1;
-        } else {
+        }else{
             right = mid;
         }
     }
     return left;
 };
 
-const check = (ranks, time) => {   
-    let total = 0;
-    for(let r of ranks){
-        total += Math.floor(Math.sqrt(time / r));
+const getSum = (r,time) =>{
+    let sum = 0;
+    for(let i=0; i<r.length; i++){
+        sum += Math.floor(Math.sqrt(time/r[i]));
     }
-    return total;
-};
+    return sum;
+}
+
