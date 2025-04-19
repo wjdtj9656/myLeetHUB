28f102cf409c7aec73b85889f2c120a158a8d7be
wjdtj9656 <wjdtj9656@naver.com>
2025-04-19 12:55:07 +0900

    Time: 0 ms (100%), Space: 56.8 MB (91.82%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,14 +4,15 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length-1;
-    
-    while(left <= right){
-        let mid = parseInt((left + right)/2);
-        if(nums[mid] === target) return mid;
-        else if(nums[mid] > target) right = mid-1;
-        else left = mid+1;
+  let left = 0;
+  let right = nums.length;
+  while(left < right){
+    let mid = Math.floor((left+right)/2);
+    if(target > nums[mid]){
+        left = mid + 1;
+    }else{
+        right = mid;
     }
-    return -1;
+  }
+  return nums[left] == target ? left : -1;
 };
