f555ad9d62073cf60f71f2c14f59deec2c78c31c
wjdtj9656 <wjdtj9656@naver.com>
2025-05-03 18:55:18 +0900

    Time: 13 ms (12.5%), Space: 65.1 MB (14.58%) - LeetHub

---
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} tops
+ * @param {number[]} bottoms
+ * @return {number}
+ */
+var minDominoRotations = function(tops, bottoms) {
+    const map = {};
+    const n = tops.length;
+    for(let i=0; i<n; i++){
+        map[tops[i]] = (map[tops[i]] || 0) + 1;
+    }
+    for(let i=0; i<n; i++){
+        map[bottoms[i]] = (map[bottoms[i]] || 0) + 1;
+    }
+    const arr = [];
+    for(let [key,value] of Object.entries(map)){
+        if(value >= n){
+            arr.push(key);
+        }
+    }
+    const ans = {};
+    let res = 10e9;
+    for(let num of arr){
+        let t = 0;
+        let b = 0;
+        for(let i=0; i<n; i++){
+            if(tops[i] != num){
+                t++;
+            }
+            if(bottoms[i] != num){
+                b++;
+            }
+            if(tops[i] == num || bottoms[i] == num){
+            }
+            else{
+                break;
+            }
+            if(i==n-1){
+                res = Math.min(res, t,b,n-(Math.min(t,b)));
+            }
+        }
+    }
+    return res ==10e9? -1:res;
+};
