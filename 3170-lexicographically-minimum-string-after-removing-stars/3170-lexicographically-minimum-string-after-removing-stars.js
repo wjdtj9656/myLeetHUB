5d4a49ea924af12dbb9352678f7c14dfc797c16e
wjdtj9656 <wjdtj9656@naver.com>
2025-06-07 18:26:24 +0900

    Time: 50 ms (80%), Space: 65.9 MB (80%) - LeetHub

--- 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.js
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.js
@@ -1,19 +1,18 @@
 var clearStars = function (s) {
-    const cnt = Array(26)
-        .fill()
-        .map(() => []);
-    const arr = s.split("");
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== "*") {
-            cnt[arr[i].charCodeAt(0) - "a".charCodeAt(0)].push(i);
-        } else {
-            for (let j = 0; j < 26; j++) {
-                if (cnt[j].length > 0) {
-                    arr[cnt[j].pop()] = "*";
+    const cnt = new Array(26).fill().map(()=>[]);
+    const str = s.split("");
+    for(let i=0; i<str.length; i++){
+        const char = str[i];
+        if(char == '*'){
+            for(let j=0; j<26; j++){
+                if(cnt[j].length > 0){
+                    str[cnt[j].pop()] = '*';
                     break;
                 }
             }
+        }else{
+            cnt[char.charCodeAt(0) - 'a'.charCodeAt(0)].push(i);
         }
     }
-    return arr.filter((c) => c !== "*").join("");
+    return str.filter(c=>c!='*').join("");
 };
