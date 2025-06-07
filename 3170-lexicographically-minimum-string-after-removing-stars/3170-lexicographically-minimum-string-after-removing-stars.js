d8c13e501548a063a8a06f6cddbf297d78464cba
wjdtj9656 <wjdtj9656@naver.com>
2025-06-07 18:39:05 +0900

    Time: 49 ms (80%), Space: 66.5 MB (80%) - LeetHub

--- 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.js
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.js
@@ -1,9 +1,10 @@
 var clearStars = function (s) {
     const cnt = new Array(26).fill().map(()=>[]);
     const str = s.split("");
+
     for(let i=0; i<str.length; i++){
         const char = str[i];
-        if(char == '*'){
+        if(char =='*'){
             for(let j=0; j<26; j++){
                 if(cnt[j].length > 0){
                     str[cnt[j].pop()] = '*';
@@ -11,7 +12,7 @@ var clearStars = function (s) {
                 }
             }
         }else{
-            cnt[char.charCodeAt(0) - 'a'.charCodeAt(0)].push(i);
+            cnt[char.charCodeAt(0) - 97].push(i);
         }
     }
     return str.filter(c=>c!='*').join("");
