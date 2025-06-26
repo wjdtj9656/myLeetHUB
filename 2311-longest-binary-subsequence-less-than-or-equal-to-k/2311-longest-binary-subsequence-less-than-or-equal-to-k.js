b527940d6aab157e817046daf31f27cd7a6efa6f
wjdtj9656 <wjdtj9656@naver.com>
2025-06-26 20:32:56 +0900

    Time: 5 ms (0%), Space: 56.1 MB (18.18%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.js
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.js
@@ -1,12 +1,16 @@
 var longestSubsequence = function(s, k) {
     let ans = 0;
-    let value = 0;
+    let value = 0n;
+    const K = BigInt(k);
     for (let i = s.length - 1; i >= 0; i--) {
         if (s[i] === '0') {
             ans++;
-        } else if (value + (1 << ans) <= k) {
-            value += (1 << ans);
-            ans++;
+        } else {
+            const add = 1n << BigInt(ans);
+            if (value + add <= K) {
+                value += add;
+                ans++;
+            }
         }
     }
     return ans;
