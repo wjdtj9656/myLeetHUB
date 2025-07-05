98f3fcca506b613ffa4ebbf4b612011117369688
wjdtj9656 <wjdtj9656@naver.com>
2025-07-05 13:22:29 +0900

    Time: 3 ms (35.71%), Space: 57.4 MB (42.86%) - LeetHub

--- 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.js
+++ 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.js
@@ -1,15 +1,21 @@
-var kthCharacter = function (k, operations) {
-    let ans = 0;
-    k = BigInt(k - 1);
-    let i = 0n;
-    while ((1n << i) <= k) i++;
-    i--;
-
-    for (; i >= 0n; i--) {
-        if ((k >> i) & 1n) {
-            ans += operations[Number(i)];
-        }
+function kthCharacter(k, operations) {
+  const n = operations.length;
+  const initialK = BigInt(k);
+  const len = [1n];
+  for (let i = 0; i < n; i++) {
+    let l = len[i] * 2n;
+    if (l > initialK) l = initialK;
+    len.push(l);
+  }
+  let idx = initialK;
+  let shift = 0;
+  for (let i = n - 1; i >= 0; i--) {
+    const L = len[i];
+    if (idx > L) {
+      idx -= L;
+      if (operations[i] === 1) shift = (shift + 1) % 26;
     }
-
-    return String.fromCharCode("a".charCodeAt(0) + (ans % 26));
-};
+  }
+  const code = (shift) % 26;
+  return String.fromCharCode('a'.charCodeAt(0) + code);
+}
