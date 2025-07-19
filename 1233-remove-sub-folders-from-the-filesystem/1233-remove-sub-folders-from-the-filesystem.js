1729f759f7a5acdab26747fd9af8806c16d0508a
wjdtj9656 <wjdtj9656@naver.com>
2025-07-19 17:57:45 +0900

    Time: 25 ms (100%), Space: 69.1 MB (83.72%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.js
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.js
@@ -1,20 +1,10 @@
-/**
- * @param {string[]} folder
- * @return {string[]}
- */
 var removeSubfolders = function(folder) {
-    folder = folder.sort((a,b)=>a.length - b.length);
-    const set = new Set();
-    for(let val of folder){
-        const arr = [...set];
-        let flag = true;
-        for(let i=0; i<arr.length; i++){
-            if(val.startsWith(arr[i]+"/")){
-                flag = false
-                break;
-            }
+    folder.sort();
+    const res = [];
+    for (let path of folder) {
+        if (res.length === 0 || !path.startsWith(res[res.length - 1] + "/")) {
+            res.push(path);
         }
-        if(flag) set.add(val);
     }
-    return [...set]
+    return res;
 };
