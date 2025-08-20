22be4660e40434572046cb0487dde62450d61ef3
wjdtj9656 <wjdtj9656@naver.com>
2025-08-20 23:38:50 +0900

    Time: 14 ms (47.22%), Space: 58.8 MB (73.61%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.js
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.js
@@ -1,22 +1,20 @@
-var countSquares = function(matrix) {
-    
-    dp_table = [...matrix];
-    
-    let [h, w]=[matrix.length, matrix[0].length];
-    
-    let counter = 0;
-    
-    for( let y=0 ; y < h ; y++){
-        for( let x=0 ; x < w ; x++){
-            
-            if( y && x && matrix[y][x] ){
-                dp_table[y][x] = 1 + Math.min( dp_table[y][x-1], dp_table[y-1][x],dp_table[y-1][x-1] );
-            } 
-            
-            counter += matrix[y][x];
-        }
-    }
-    
-    return counter;
+function countSquares(matrix) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  const dp = Array.from({ length: m }, () => Array(n).fill(0));
+  let total = 0;
 
-};
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === 1) {
+        if (i === 0 || j === 0) {
+          dp[i][j] = 1;
+        } else {
+          dp[i][j] = 1 + Math.min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]);
+        }
+        total += dp[i][j];
+      }
+    }
+  }
+  return total;
+}
