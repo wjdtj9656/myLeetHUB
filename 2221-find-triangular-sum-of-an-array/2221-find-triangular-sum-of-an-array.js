7e73385aa4f32afba71dd4c05355c2809098a945
wjdtj9656 <wjdtj9656@naver.com>
2025-09-30 13:31:45 +0900

    Time: 41 ms (87.5%), Space: 55.5 MB (86.67%) - LeetHub

---
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
@@ -0,0 +1,9 @@
+var triangularSum = function(nums) {
+    while(nums.length > 1){
+      for(let i=0; i<nums.length-1; i++){
+        nums[i] = (nums[i] + nums[i+1]) % 10;
+      }
+      nums.pop();
+    }
+    return nums[0];
+};
