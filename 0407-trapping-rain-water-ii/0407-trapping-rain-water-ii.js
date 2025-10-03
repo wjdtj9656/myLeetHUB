a8ec423ce202d7363e73ffd8d437ea69648c01a0
wjdtj9656 <wjdtj9656@naver.com>
2025-10-03 21:23:21 +0900

    Time: 109 ms (69.77%), Space: 73.6 MB (30.23%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.js
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.js
@@ -1,114 +1,68 @@
 function trapRainWater(heightMap) {
-  if (!heightMap || heightMap.length === 0 || heightMap[0].length === 0) {
-    return 0;
-  }
-
   const m = heightMap.length;
   const n = heightMap[0].length;
+  if (m < 3 || n < 3) return 0;
 
-  const visited = Array.from({ length: m }, () => Array(n).fill(false));
-
-  const minHeap = [];
-
-  function heapPush(item) {
-    minHeap.push(item);
-    let curIndex = minHeap.length - 1;
-
-    while (curIndex > 0) {
-      const parentIndex = Math.floor((curIndex - 1) / 2);
-
-      if (minHeap[curIndex][0] < minHeap[parentIndex][0]) {
-        [minHeap[curIndex], minHeap[parentIndex]] =
-          [minHeap[parentIndex], minHeap[curIndex]];
-        curIndex = parentIndex;
-      } else {
-        break;
+  class MinHeap {
+    constructor() { this.a = []; }
+    size() { return this.a.length; }
+    push(v) {
+      this.a.push(v);
+      let i = this.a.length - 1;
+      while (i > 0) {
+        const p = (i - 1) >> 1;
+        if (this.a[p][0] <= this.a[i][0]) break;
+        [this.a[p], this.a[i]] = [this.a[i], this.a[p]];
+        i = p;
       }
+    }
+    pop() {
+      const res = this.a[0];
+      const x = this.a.pop();
+      if (this.a.length) {
+        this.a[0] = x;
+        let i = 0;
+        while (true) {
+          let l = i * 2 + 1, r = l + 1, s = i;
+          if (l < this.a.length && this.a[l][0] < this.a[s][0]) s = l;
+          if (r < this.a.length && this.a[r][0] < this.a[s][0]) s = r;
+          if (s === i) break;
+          [this.a[s], this.a[i]] = [this.a[i], this.a[s]];
+          i = s;
+        }
+      }
+      return res;
     }
   }
 
-  function heapPop() {
-    if (minHeap.length === 0) return null;
-
-    const top = minHeap[0];
-    const end = minHeap.pop();
-
-    if (minHeap.length > 0) {
-      minHeap[0] = end;
-
-      let curIndex = 0;
-      while (true) {
-        let leftIndex = 2 * curIndex + 1;
-        let rightIndex = 2 * curIndex + 2;
-        let smallestIndex = curIndex;
-
-        if (
-          leftIndex < minHeap.length &&
-          minHeap[leftIndex][0] < minHeap[smallestIndex][0]
-        ) {
-          smallestIndex = leftIndex;
-        }
-        if (
-          rightIndex < minHeap.length &&
-          minHeap[rightIndex][0] < minHeap[smallestIndex][0]
-        ) {
-          smallestIndex = rightIndex;
-        }
-        if (smallestIndex === curIndex) break;
-
-        [minHeap[curIndex], minHeap[smallestIndex]] =
-          [minHeap[smallestIndex], minHeap[curIndex]];
-        curIndex = smallestIndex;
-      }
-    }
-
-    return top;
-  }
-
+  const heap = new MinHeap();
+  const vis = Array.from({ length: m }, () => Array(n).fill(false));
   for (let i = 0; i < m; i++) {
-    heapPush([heightMap[i][0], i, 0]);
-    visited[i][0] = true;
-
-    heapPush([heightMap[i][n - 1], i, n - 1]);
-    visited[i][n - 1] = true;
+    heap.push([heightMap[i][0], i, 0]);
+    heap.push([heightMap[i][n - 1], i, n - 1]);
+    vis[i][0] = true;
+    vis[i][n - 1] = true;
+  }
+  for (let j = 1; j < n - 1; j++) {
+    heap.push([heightMap[0][j], 0, j]);
+    heap.push([heightMap[m - 1][j], m - 1, j]);
+    vis[0][j] = true;
+    vis[m - 1][j] = true;
   }
 
-  for (let j = 0; j < n; j++) {
-    heapPush([heightMap[0][j], 0, j]);
-    visited[0][j] = true;
-
-    heapPush([heightMap[m - 1][j], m - 1, j]);
-    visited[m - 1][j] = true;
-  }
-
-  let totalWater = 0;
-
-  const directions = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
-
-  while (minHeap.length > 0) {
-    const [curHeight, r, c] = heapPop();
-
-    for (const [dr, dc] of directions) {
-      const nr = r + dr;
-      const nc = c + dc;
-
-      if (nr < 0 || nr >= m || nc < 0 || nc >= n || visited[nr][nc]) continue;
-
-      visited[nr][nc] = true;
-
-      if (heightMap[nr][nc] < curHeight) {
-        totalWater += curHeight - heightMap[nr][nc];
-        heapPush([curHeight, nr, nc]);
-      } else {
-        heapPush([heightMap[nr][nc], nr, nc]);
-      }
+  let ans = 0, maxh = 0;
+  const dirs = [[1,0],[-1,0],[0,1],[0,-1]];
+  while (heap.size()) {
+    const [h, r, c] = heap.pop();
+    if (h > maxh) maxh = h;
+    for (const [dr, dc] of dirs) {
+      const nr = r + dr, nc = c + dc;
+      if (nr < 0 || nr >= m || nc < 0 || nc >= n || vis[nr][nc]) continue;
+      vis[nr][nc] = true;
+      const nh = heightMap[nr][nc];
+      if (nh < maxh) ans += maxh - nh;
+      heap.push([Math.max(nh, maxh), nr, nc]);
     }
   }
-
-  return totalWater;
+  return ans;
 }
