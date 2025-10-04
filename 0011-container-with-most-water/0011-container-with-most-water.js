9e769b8ec8afa6ed8ea976bd07a930426f1d1a63
wjdtj9656 <wjdtj9656@naver.com>
2025-10-04 23:41:18 +0900

    Time: 53 ms (5.12%), Space: 68.6 MB (5.1%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,14 +3,15 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let left = 0, right = height.length-1;
+    let left = 0;
+    let right = height.length-1;
     let result = 0;
-    while(left <= right){
-        result = Math.max(result,(right-left) * Math.min(height[left],height[right]));
-        if(height[left] <= height[right]){
-            left++;
+    while(left < right){
+        result = Math.max(result, (right-left) * Math.min(height[left],height[[right]]));
+        if(height[right] < height[left]){
+            right--;
         }
-        else right--;
+        else left++;
     }
     return result;
 };
