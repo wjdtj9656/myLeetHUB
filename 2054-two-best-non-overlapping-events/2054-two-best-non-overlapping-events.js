026954f12ce619bd31dc0e73b99b921ac4b1d707
wjdtj9656 <wjdtj9656@naver.com>
2025-12-23 22:40:24 +0900

    Time: 76 ms (71.43%), Space: 93.1 MB (21.43%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.js
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.js
@@ -4,34 +4,38 @@
  */
 var maxTwoEvents = function(events) {
     const n = events.length;
-    events.sort((a,b)=>a[1]-b[1]);
-    const arr = new Array(n).fill(0);
-    arr[0] = events[0][2];
-    for (let i=1; i<n; i++) {
-        arr[i] = Math.max(arr[i-1], events[i][2]);
+    events.sort((a, b) => a[0] - b[0]);
+
+    const suffixMax = new Array(n);
+    suffixMax[n - 1] = events[n - 1][2];
+    
+    for (let i = n - 2; i >= 0; i--) {
+        suffixMax[i] = Math.max(events[i][2], suffixMax[i + 1]);
     }
-    const starts = events.map(e=>e[0]);
-    const search = (target) =>{
-        let left = 0;
-        let right = n;
-        while(left < right){
-            const mid = Math.floor((left+right)/2);
-            if(events[mid][1] < target){
-                left = mid+1;
-            }
-            else{
-                right = mid;
+
+    let maxVal = 0;
+
+    for (let i = 0; i < n; i++) {
+        let currentVal = events[i][2];
+        maxVal = Math.max(maxVal, currentVal);
+
+        let left = i + 1, right = n - 1;
+        let nextEventIdx = -1;
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (events[mid][0] > events[i][1]) {
+                nextEventIdx = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        return left;
-    }
-    let answer = 0;
-    answer = Math.max(...arr);
 
-    for(let i=0; i<n; i++){
-        const nextIndex = search(events[i][0])-1;
-        const val = events[i][2] + (nextIndex >= 0? arr[nextIndex] : 0);
-        answer = Math.max(answer, val);
+        if (nextEventIdx !== -1) {
+            maxVal = Math.max(maxVal, currentVal + suffixMax[nextEventIdx]);
+        }
     }
-    return answer;
-}
+
+    return maxVal;
+};
