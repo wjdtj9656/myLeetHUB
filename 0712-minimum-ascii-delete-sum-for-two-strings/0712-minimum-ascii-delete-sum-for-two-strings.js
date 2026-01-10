d871f961171b48667bfaaf01c39534b96a1c0dd7
wjdtj9656 <wjdtj9656@naver.com>
2026-01-10 16:23:02 +0900

    Time: 24 ms (87.5%), Space: 63.5 MB (27.5%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.js
@@ -0,0 +1,28 @@
+const minimumDeleteSum = function(s1, s2) {
+    const n = s1.length;
+    const m = s2.length;
+    const dp = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(0));
+
+    for (let i = 1; i <= n; i++) {
+        dp[i][0] = dp[i - 1][0] + s1.charCodeAt(i - 1);
+    }
+
+    for (let j = 1; j <= m; j++) {
+        dp[0][j] = dp[0][j - 1] + s2.charCodeAt(j - 1);
+    }
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= m; j++) {
+            if (s1[i - 1] === s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.min(
+                    dp[i - 1][j] + s1.charCodeAt(i - 1),
+                    dp[i][j - 1] + s2.charCodeAt(j - 1)
+                );
+            }
+        }
+    }
+
+    return dp[n][m];
+};
