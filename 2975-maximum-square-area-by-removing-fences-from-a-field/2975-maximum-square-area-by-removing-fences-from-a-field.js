42b73fad7272b39d4b55224a611cf337c758af16
wjdtj9656 <wjdtj9656@naver.com>
2026-01-16 13:26:13 +0900

    Time: 470 ms (90%), Space: 86 MB (90%) - LeetHub

---
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[]} hFences
+ * @param {number[]} vFences
+ * @return {number}
+ */
+var maximizeSquareArea = function(m, n, hFences, vFences) {
+    hFences.push(1, m);
+    vFences.push(1, n);
+    
+    hFences.sort((a, b) => a - b);
+    vFences.sort((a, b) => a - b);
+    
+    const hGaps = new Set();
+    
+    for (let i = 0; i < hFences.length; i++) {
+        for (let j = i + 1; j < hFences.length; j++) {
+            hGaps.add(hFences[j] - hFences[i]);
+        }
+    }
+    
+    let maxGap = -1;
+    
+    for (let i = 0; i < vFences.length; i++) {
+        for (let j = i + 1; j < vFences.length; j++) {
+            const currentGap = vFences[j] - vFences[i];
+            if (hGaps.has(currentGap)) {
+                if (currentGap > maxGap) {
+                    maxGap = currentGap;
+                }
+            }
+        }
+    }
+    
+    if (maxGap === -1) {
+        return -1;
+    }
+    
+    return Number((BigInt(maxGap) * BigInt(maxGap)) % 1000000007n);
+};
