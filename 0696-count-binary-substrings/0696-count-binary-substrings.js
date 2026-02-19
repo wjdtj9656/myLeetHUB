96cbcf236c78eee105a76ce325dcb3280126d42a
wjdtj9656 <wjdtj9656@naver.com>
2026-02-19 12:28:50 +0900

    Time: 11 ms (42.59%), Space: 58.3 MB (86.11%) - LeetHub

---
+++ 0696-count-binary-substrings/0696-count-binary-substrings.js
@@ -0,0 +1,19 @@
+const countBinarySubstrings = function(s) {
+    let prev = 0;
+    let curr = 1;
+    let count = 0;
+
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === s[i - 1]) {
+            curr++;
+        } else {
+            count += Math.min(prev, curr);
+            prev = curr;
+            curr = 1;
+        }
+    }
+    
+    count += Math.min(prev, curr);
+
+    return count;
+};
