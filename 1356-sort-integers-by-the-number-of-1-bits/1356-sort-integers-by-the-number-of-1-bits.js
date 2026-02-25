44518d136cde1b7b2523ab235ffb56e4a35ec367
wjdtj9656 <wjdtj9656@naver.com>
2026-02-25 13:26:20 +0900

    Time: 3 ms (98.21%), Space: 57.3 MB (82.14%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
@@ -1,13 +1,12 @@
-/**
- * @param {number[]} arr
- * @return {number[]}
- */
 var sortByBits = function(arr) {
-    arr.sort((a,b)=>{
-        let aNum = a.toString(2).split('').filter(v=>v==1);
-        let bNum = b.toString(2).split('').filter(v=>v==1);
-        if(aNum.length == bNum.length) return a-b;
-        return aNum.length - bNum.length
-    })
-    return arr
+    const getBits = (num) => {
+        let count = 0;
+        while (num > 0) {
+            num &= (num - 1);
+            count++;
+        }
+        return count;
+    };
+    
+    return arr.sort((a, b) => getBits(a) - getBits(b) || a - b);
 };
