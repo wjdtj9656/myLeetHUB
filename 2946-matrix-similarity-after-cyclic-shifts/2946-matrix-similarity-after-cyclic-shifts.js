d9fd2de2d0e2379785f5a35d96c44ac139db9ca3
wjdtj9656 <wjdtj9656@naver.com>
2026-03-27 15:06:39 +0900

    Time: 0 ms (100%), Space: 55.6 MB (89.66%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.js
@@ -0,0 +1,21 @@
+var areSimilar = function(mat, k) {
+    const m = mat.length;
+    const n = mat[0].length;
+    const shift = k % n;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i % 2 === 0) {
+                if (mat[i][j] !== mat[i][(j + shift) % n]) {
+                    return false;
+                }
+            } else {
+                if (mat[i][j] !== mat[i][(j - shift + n) % n]) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+};
