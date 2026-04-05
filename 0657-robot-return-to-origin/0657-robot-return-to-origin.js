197730dbfa985e7727769a039b88e08eb4f028fb
wjdtj9656 <wjdtj9656@naver.com>
2026-04-05 11:11:10 +0900

    Time: 5 ms (80.27%), Space: 55.9 MB (63.55%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -0,0 +1,20 @@
+var judgeCircle = function(moves) {
+    let x = 0;
+    let y = 0;
+
+    for (let i = 0; i < moves.length; i++) {
+        const move = moves[i];
+        
+        if (move === 'U') {
+            y++;
+        } else if (move === 'D') {
+            y--;
+        } else if (move === 'L') {
+            x--;
+        } else if (move === 'R') {
+            x++;
+        }
+    }
+
+    return x === 0 && y === 0;
+};
