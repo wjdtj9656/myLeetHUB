340f3ef65b61fba1b22cb8866e08c9d9646d7d35
wjdtj9656 <wjdtj9656@naver.com>
2026-05-05 12:11:20 +0900

    Time: 0 ms (100%), Space: 53.8 MB (81.71%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -2,7 +2,7 @@ var rotate = function(matrix) {
     const n = matrix.length;
     
     for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
+        for (let j = i; j < n; j++) {
             let temp = matrix[i][j];
             matrix[i][j] = matrix[j][i];
             matrix[j][i] = temp;
