f3182950761a962a4967946b945a606782d8f85b
wjdtj9656 <wjdtj9656@naver.com>
2026-05-22 13:28:55 +0900

    Time: 0 ms (100%), Space: 53.8 MB (56.22%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -1,24 +1,28 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
-var search = function(nums, target) {
-    let arr = [...nums];
-    arr.sort((a,b)=>a-b);
+function search(nums, target) {
     let left = 0;
-    let right = arr.length-1;
-    while(left <= right){
-        let mid = (left + right) >> 1;
-        if(arr[mid] == target){
-            return nums.indexOf(target);
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
         }
-        else if(arr[mid] < target){
-            left = mid + 1;
-        }
-        else if(arr[mid] > target){
-            right = mid - 1;
+
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
     }
+
     return -1;
-};
+}
